1b3e05eceb703ec9e588b7f67549f2d05bb15e24
Fabio <fabio.ottaviani@gmail.com>
2017-03-20 15:38:16 +0100

    Aggiornate booleane dei data-attributes per validazione come stringhe

--- socialwall/js/socialwall.js
+++ socialwall/js/socialwall.js
@@ -9,142 +9,144 @@ SocialWall
 --------------------------------------------------*/
 var $socialWallGrid = $('.social-wall-grid'),
     socialWall = {
-    codiceSocialWall: 'b17d6114643f2627f53de462565fda5e',
-    accessToken: '634737799ba786bbfe74ff48a52b3f54725782e5eab78',
-    maxFeed: $socialWallGrid.attr('data-max-feed') || 10,
-    withMasonry: $socialWallGrid.attr('data-masonry') || false,
-    loadMore: $socialWallGrid.attr('data-load-more') || false,
-    actualFeed: 0,
-    tooManyFeeds: false,
-    $data: null,
-    init: function () {
-        $.ajax({
-            url: 'http://www.seejay.co/api/v1.0/wall/content/' + socialWall.codiceSocialWall + '?access_token=' + socialWall.accessToken,
-            dataType: 'json',
-            sync: true,
-            cache: false,
-            crossDomain: true,
-            success: function (data) {
-                socialWall.$data = data;
+        codiceSocialWall: 'b17d6114643f2627f53de462565fda5e',
+        accessToken: '634737799ba786bbfe74ff48a52b3f54725782e5eab78',
+        maxFeed: $socialWallGrid.attr('data-max-feed') || 10,
+        withMasonry: $socialWallGrid.attr('data-masonry') || 'false',
+        loadMore: $socialWallGrid.attr('data-load-more') || 'false',
+        actualFeed: 0,
+        tooManyFeeds: false,
+        $data: null,
+        init: function () {
+            $.ajax({
+                url: 'http://www.seejay.co/api/v1.0/wall/content/' + socialWall.codiceSocialWall + '?access_token=' + socialWall.accessToken,
+                dataType: 'json',
+                sync: true,
+                cache: false,
+                crossDomain: true,
+                success: function (data) {
+                    socialWall.$data = data;
 
-                if (socialWall.withMasonry) {
-                    socialWall.masonry();
-                }
-
-                socialWall.createGrid(socialWall.actualFeed, socialWall.maxFeed);
-
-            }
-        });
-    },
-    createGrid: function (from, to) {
-
-        if (socialWall.$data.data.items.length < socialWall.maxFeed) {
-            socialWall.maxFeed = socialWall.$data.data.items.length;
-            socialWall.tooManyFeeds = true;
-        }
-
-        for (i = from; i < to; i++) {
-
-            if (typeof socialWall.$data.data.items[i] != 'undefined') {
-
-                var feed = socialWall.$data.data.items[i],
-                    $box = $('<div class="grid-item"><div class="grid-wrap"><div class="box"></div></div></div>'),
-                    source = feed.source,
-                    feedID = feed.id,
-                    date = feed.date,
-                    text = urlify(feed.text),
-                    media = feed.media || undefined,
-                    user = feed.user,
-                    avatar = user.avatar.replace('https', 'http'),
-                    id = user.id,
-                    name = user.name,
-                    icon,
-                    socialUrl;
-
-                switch (source) {
-                    case 'PT':
-                        icon = '<i class="fa fa-pinterest" aria-hidden="true"></i>';
-                        socialUrl = 'https://www.pinterest.com/';
-                        break;
-                    case 'IG':
-                        icon = '<i class="fa fa-instagram" aria-hidden="true"></i>';
-                        socialUrl = 'http://instagram.com/';
-                        break;
-                    case 'FB':
-                        icon = '<i class="fa fa-facebook" aria-hidden="true"></i>';
-                        socialUrl = 'http://facebook.com/';
-                        break;
-                    case 'TW':
-                        icon = '<i class="fa fa-twitter" aria-hidden="true"></i>';
-                        socialUrl = 'https://twitter.com/';
-                        break;
-                }
-
-                $('.box', $box).addClass('box-' + source);
-
-                // Se non è Google Plus e contiene foto
-                if (source != 'GP' && typeof media != 'undefined') {
-
-                    // media
-                    $media = $('<div class="media"><figure><img src="' + media.url + '" /></figure></div>').appendTo($('.box', $box));
-
-                    // author 
-                    $avatar = $('<div class="author"><a href="' + (socialUrl + id) + '" target="_blank"><figure class="img"><img src="' + avatar + '" onError="$(this).remove();" class="avatar" /></figure><span class="author-name">' + name + '</span> <span class="author-username">' + id + '</span></a></div>').appendTo($('.box', $box));;
-
-                    // text
-                    $text = $('<div class="text"></div>').html(text).appendTo($('.box', $box));
-
-                    // extra
-                    $source = $('<div class="source"><span class="source">' + icon + '</span></div>').appendTo($('.box', $box));
-
-                    // append
-                    $box.appendTo($socialWallGrid);
-
-                    if (socialWall.withMasonry) {
-                        $wall.masonry('appended', $box);
-                        $wall.imagesLoaded().progress(function () {
-                            $wall.masonry('layout');
-                        });
+                    if (socialWall.withMasonry === 'true') {
+                        socialWall.masonry();
                     }
 
-                    socialWall.actualFeed++;
+                    socialWall.createGrid(socialWall.actualFeed, socialWall.maxFeed);
 
-                } else {
-                    if (!socialWall.tooManyFeeds) {
-                        socialWall.maxFeed++;
-                    }
                 }
+            });
+        },
+        createGrid: function (from, to) {
 
+            if (socialWall.$data.data.items.length < socialWall.maxFeed) {
+                socialWall.maxFeed = socialWall.$data.data.items.length;
+                socialWall.tooManyFeeds = true;
             }
-        }
 
-    },
-    masonry: function () {
-        $wall = $socialWallGrid.masonry({
-            itemSelector: '.grid-item'
-        });
+            for (i = from; i < to; i++) {
 
-        $wall.imagesLoaded().progress(function () {
-            $wall.masonry('layout');
-        });
+                if (typeof socialWall.$data.data.items[i] != 'undefined') {
 
-        if (socialWall.loadMore == true) {
-            $('<div class="social-wall-load-more"><button class="btn btn-load-more">Load more</button></div>').insertAfter($socialWallGrid);
-        }
+                    var feed = socialWall.$data.data.items[i],
+                        $box = $('<div class="grid-item"><div class="grid-wrap"><div class="box"></div></div></div>'),
+                        source = feed.source,
+                        feedID = feed.id,
+                        date = feed.date,
+                        text = urlify(feed.text),
+                        media = feed.media || undefined,
+                        user = feed.user,
+                        avatar = user.avatar.replace('https', 'http'),
+                        id = user.id,
+                        name = user.name,
+                        icon,
+                        socialUrl;
 
-        $(document).on('click', '.btn-load-more', function () {
-            socialWall.createGrid(socialWall.actualFeed, socialWall.actualFeed * 2);
-            setTimeout(function () {
+                    switch (source) {
+                        case 'PT':
+                            icon = '<i class="fa fa-pinterest" aria-hidden="true"></i>';
+                            socialUrl = 'https://www.pinterest.com/';
+                            break;
+                        case 'IG':
+                            icon = '<i class="fa fa-instagram" aria-hidden="true"></i>';
+                            socialUrl = 'http://instagram.com/';
+                            break;
+                        case 'FB':
+                            icon = '<i class="fa fa-facebook" aria-hidden="true"></i>';
+                            socialUrl = 'http://facebook.com/';
+                            break;
+                        case 'TW':
+                            icon = '<i class="fa fa-twitter" aria-hidden="true"></i>';
+                            socialUrl = 'https://twitter.com/';
+                            break;
+                    }
+
+                    $('.box', $box).addClass('box-' + source);
+
+                    // Se non è Google Plus e contiene foto
+                    if (source != 'GP' && typeof media != 'undefined') {
+
+                        // media
+                        $media = $('<div class="media"><figure><img src="' + media.url + '" /></figure></div>').appendTo($('.box', $box));
+
+                        // author 
+                        $avatar = $('<div class="author"><a href="' + (socialUrl + id) + '" target="_blank"><figure class="img"><img src="' + avatar + '" onError="$(this).remove();" class="avatar" /></figure><span class="author-name">' + name + '</span> <span class="author-username">' + id + '</span></a></div>').appendTo($('.box', $box));;
+
+                        // text
+                        $text = $('<div class="text"></div>').html(text).appendTo($('.box', $box));
+
+                        // extra
+                        $source = $('<div class="source"><span class="source">' + icon + '</span></div>').appendTo($('.box', $box));
+
+                        // append
+                        $box.appendTo($socialWallGrid);
+
+                        if (socialWall.withMasonry === 'true') {
+                            $wall.masonry('appended', $box);
+                            $wall.imagesLoaded().progress(function () {
+                                $wall.masonry('layout');
+                            });
+                        }
+
+                        socialWall.actualFeed++;
+
+                    } else {
+                        if (!socialWall.tooManyFeeds) {
+                            socialWall.maxFeed++;
+                        }
+                    }
+
+                }
+            }
+
+        },
+        masonry: function () {
+            $wall = $socialWallGrid.masonry({
+                itemSelector: '.grid-item'
+            });
+
+            $wall.imagesLoaded().progress(function () {
                 $wall.masonry('layout');
-            }, 300);
+            });
 
-            if (socialWall.actualFeed >= socialWall.$data.data.items.length) {
-                $('.social-wall-load-more').remove();
+
+
+            if (socialWall.loadMore === 'true') {
+                $('<div class="social-wall-load-more"><button class="btn btn-load-more">Load more</button></div>').insertAfter($socialWallGrid);
             }
-        });
 
+            $(document).on('click', '.btn-load-more', function () {
+                socialWall.createGrid(socialWall.actualFeed, socialWall.actualFeed * 2);
+                setTimeout(function () {
+                    $wall.masonry('layout');
+                }, 300);
+
+                if (socialWall.actualFeed >= socialWall.$data.data.items.length) {
+                    $('.social-wall-load-more').remove();
+                }
+            });
+
+        }
     }
-}
 
 
 /*--------------------------------------------------
